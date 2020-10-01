9f76b3129a075f4ac21c67b507b8b425b47806a0
yadavashu <55954955+yadavashu@users.noreply.github.com>
2020-10-01 11:46:36 +0530

    Update code.c

--- code.c
+++ code.c
@@ -1,4 +1,8 @@
-#include<stdio.h.>
-void main(){
-printf("Let's move on from universe to real life");
+//how to add two number in C language
+#include<stdio.h>
+int main()
+{
+  int a=4,b,6;
+  int c=a+b;
+  printf("%d",&c);
 }
